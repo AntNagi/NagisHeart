74b474fa008d597929ec05fda4026f37393a1124
AntNagi <antswift21@gmail.com>
2026-08-02 20:47:53 +0800

    Revert "web: infer story map section unlock from ending progress"
    This reverts commit f67f5a917143d91d8d9eced631643c7047ba7aeb.

--- web/src/ui/overlays/StoryMapOverlay.js
+++ web/src/ui/overlays/StoryMapOverlay.js
@@ -47,29 +47,9 @@ export class StoryMapOverlay {
   }
 
   _sectionState(chapter, index) {
-    const state = this._controller.getSectionState(
+    return this._controller.getSectionState(
       chapter.id, index, chapter.sections[index].startNode,
     );
-    if (state !== 'LOCKED') return state;
-    if (this._scopeUnlockedByEnding(chapter.sections[index].scope)) return 'COMPLETED';
-    return state;
-  }
-
-  _scopeUnlockedByEnding(scope) {
-    if (!this._endingScopes) {
-      const endings = this._controller.getUnlockedEndings();
-      this._endingScopes = new Set();
-      if (endings.has('end_true') || endings.has('end_good')) this._endingScopes.add('dream');
-      if (endings.has('end_normal')) this._endingScopes.add('stay');
-      if (endings.has('end_bad')) this._endingScopes.add('bad');
-      if (endings.size > 0) {
-        this._endingScopes.add('common');
-        this._endingScopes.add('M');
-        this._endingScopes.add('J');
-      }
-    }
-    if (!scope || scope === 'common') return this._endingScopes.has('common');
-    return this._endingScopes.has(scope);
   }
 
   _isOpen(state) {
@@ -78,8 +58,7 @@ export class StoryMapOverlay {
 
   _chapterUnlocked(chapter) {
     const visited = this._controller.getVisitedNodes();
-    if (chapter.sections.some(s => visited.has(s.startNode))) return true;
-    return chapter.sections.some(s => this._scopeUnlockedByEnding(s.scope));
+    return chapter.sections.some(s => visited.has(s.startNode));
   }
 
   _bg(nodeId) {
